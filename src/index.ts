d25ce4bad0e57d0e88521803f145cec9bc719cb5
Michael Kuckuk <mdk@kuckuk.com>
2021-08-01 15:36:20 +0200

    Add sanity checks for extensions

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,13 @@ import { Extension } from './core/Extension'
 import { Subject, tap } from 'rxjs'
 import inquirer, { Answers, DistinctQuestion } from 'inquirer'
 import { TestExtension } from './extensions/TestExtension'
+import { performSanityChecksOnExtensions } from './core/SanityChecks'
 
 const extensions: Array<Extension> = [TestExtension]
+
+// If any of these fail, the entire application will fail.
+performSanityChecksOnExtensions(extensions)
+
 const prompts$ = new Subject<DistinctQuestion<Answers>>()
 const answers$ = inquirer.prompt(prompts$).ui.process
 
