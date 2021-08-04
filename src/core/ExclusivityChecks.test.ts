caa16222643066d82526cd9857de36771c7ec812
Michael Kuckuk <mdk@kuckuk.com>
2021-08-04 17:31:20 +0200

    Create checks for exclusivities

--- src/core/ExclusivityChecks.test.ts
+++ src/core/ExclusivityChecks.test.ts
@@ -1,7 +1,69 @@
-import { checkExclusivities } from './ExclusivityChecks'
+import {
+  checkExclusivities,
+  InvalidExclusivitiesResponse,
+} from './ExclusivityChecks'
+import { generateMockExtension } from '../extensions/MockExtension'
+import { performSanityChecksOnExtensions } from './SanityChecks'
 
 describe('checkExclusivities', () => {
-  it('should exist', () => {
-    expect(typeof checkExclusivities).toBe('function')
+  const extensionA = generateMockExtension({ name: 'Extension A' })
+  const extensionB = generateMockExtension({ name: 'Extension B' })
+  const extensionC = generateMockExtension({ name: 'Extension C' })
+
+  const exclusiveToA = generateMockExtension({
+    name: 'Exclusive to A',
+    exclusiveTo: [extensionA],
+  })
+  const exclusiveToABC = generateMockExtension({
+    name: 'Exclusive to A, B, and C',
+    exclusiveTo: [extensionA, extensionB, extensionC],
+  })
+
+  describe('mock data', () => {
+    it('should be valid', () => {
+      expect(() =>
+        performSanityChecksOnExtensions([extensionA, extensionB, extensionC]),
+      ).not.toThrow()
+    })
+  })
+
+  it('should accept an empty list of extensions', () => {
+    expect(checkExclusivities([]).isValidConfiguration).toBe(true)
+  })
+
+  it('should accept a list of extensions without exclusivities', () => {
+    expect(
+      checkExclusivities([extensionA, extensionB, extensionC])
+        .isValidConfiguration,
+    ).toBe(true)
+  })
+
+  it('should NOT accept a list of extensions where one is exclusive to one of the others', () => {
+    expect(
+      checkExclusivities([extensionA, extensionB, extensionC, exclusiveToA])
+        .isValidConfiguration,
+    ).toBe(false)
+  })
+
+  it('should NOT accept a list of extensions where one is exclusive to multiple of the others', () => {
+    expect(
+      checkExclusivities([extensionA, extensionB, extensionC, exclusiveToABC])
+        .isValidConfiguration,
+    ).toBe(false)
+  })
+
+  it('should include the names of all forbidden extensions as well as its own name in the error message', () => {
+    const result = checkExclusivities([
+      extensionA,
+      extensionB,
+      extensionC,
+      exclusiveToABC,
+    ]) as InvalidExclusivitiesResponse
+
+    expect(result.isValidConfiguration).toBe(false)
+    expect(result.errorMessages[0]).toMatch(exclusiveToABC.name)
+    expect(result.errorMessages[0]).toMatch(extensionA.name)
+    expect(result.errorMessages[0]).toMatch(extensionB.name)
+    expect(result.errorMessages[0]).toMatch(extensionC.name)
   })
 })
--- src/core/ExclusivityChecks.ts
+++ src/core/ExclusivityChecks.ts
@@ -11,5 +11,31 @@ export type InvalidExclusivitiesResponse = {
 export const checkExclusivities = (
   chosenExtensions: Array<Extension>,
 ): ValidExclusivitiesResponse | InvalidExclusivitiesResponse => {
-  return { isValidConfiguration: true }
+  const errorMessages = chosenExtensions.reduce((errorMessages, extension) => {
+    const forbiddenButYetChosenExtensionNames = extension.exclusiveTo
+      ?.filter((otherExtension) => chosenExtensions.includes(otherExtension))
+      .map((extension) => extension.name)
+
+    if (forbiddenButYetChosenExtensionNames?.length) {
+      return [
+        ...errorMessages,
+        `${
+          extension.name
+        } cannot be used alongside the following extensions: ${forbiddenButYetChosenExtensionNames.join(
+          ', ',
+        )}`,
+      ]
+    } else {
+      return errorMessages
+    }
+  }, [] as string[])
+
+  if (errorMessages.length) {
+    return {
+      isValidConfiguration: false,
+      errorMessages,
+    }
+  } else {
+    return { isValidConfiguration: true }
+  }
 }
