9df36a35e6454817c8f96134e2fd1c7a47b1020f
Michael Kuckuk <mdk@kuckuk.com>
2021-08-09 00:36:07 +0200

    Insert separators between extensions

---
+++ src/core/userDialog/SelectExtensions.test.ts
@@ -0,0 +1,109 @@
+import { Subject } from 'rxjs'
+import {
+  Answers,
+  CheckboxQuestion,
+  DistinctQuestion,
+  Separator,
+} from 'inquirer'
+import { generateMockExtension } from '../../extensions/MockExtension'
+import { ExtensionCategory } from '../Extension'
+import { extensionCategoryTitles, selectExtensions } from './SelectExtensions'
+import Choice = require('inquirer/lib/objects/choice')
+
+let prompts$: Subject<DistinctQuestion>
+let answers$: Subject<Answers>
+const oneExtensionPerCategory = [
+  generateMockExtension({
+    category: ExtensionCategory.CSS_PREPROCESSOR,
+  }),
+  generateMockExtension({
+    category: ExtensionCategory.FRONTEND_FRAMEWORK,
+  }),
+  generateMockExtension({
+    category: ExtensionCategory.ONLY_FOR_TESTING,
+  }),
+  generateMockExtension({
+    category: ExtensionCategory.JAVASCRIPT_FLAVOR,
+  }),
+]
+type Separator = { line: string; type: 'separator' }
+
+beforeEach(() => {
+  prompts$ = new Subject()
+  answers$ = new Subject()
+})
+
+it('should always ensure correct order of categories (Only for Testing, JS flavor, framework, CSS pre-processor)', (done) => {
+  prompts$.subscribe((question) => {
+    const choices = (question as CheckboxQuestion).choices as Array<
+      Choice | Separator
+    >
+    const separators = choices.filter(
+      (choice): choice is Separator => choice instanceof Separator,
+    )
+
+    // Enums are transpiled to objects containing name: value, but also value: name
+    // Therefore, each actual enum value appears twice in the transpiled object
+    expect(separators).toHaveLength(Object.keys(ExtensionCategory).length / 2)
+    expect(separators[0].line).toMatch(
+      extensionCategoryTitles[ExtensionCategory.ONLY_FOR_TESTING],
+    )
+    expect(separators[1].line).toMatch(
+      extensionCategoryTitles[ExtensionCategory.JAVASCRIPT_FLAVOR],
+    )
+    expect(separators[2].line).toMatch(
+      extensionCategoryTitles[ExtensionCategory.FRONTEND_FRAMEWORK],
+    )
+    expect(separators[3].line).toMatch(
+      extensionCategoryTitles[ExtensionCategory.CSS_PREPROCESSOR],
+    )
+
+    done()
+  })
+
+  selectExtensions(prompts$, answers$, oneExtensionPerCategory)
+})
+
+it('should insert a separator before the first question', (done) => {
+  prompts$.subscribe((question) => {
+    const choices = (question as CheckboxQuestion).choices as Array<
+      Choice | Separator
+    >
+    expect(choices[0]).toHaveProperty('type', 'separator')
+    expect(choices[1]).not.toHaveProperty('type', 'separator')
+
+    done()
+  })
+
+  selectExtensions(prompts$, answers$, oneExtensionPerCategory)
+})
+
+it('should insert a separator between two categories', (done) => {
+  const extensions = [
+    // Separator, index = 0
+    generateMockExtension({ category: ExtensionCategory.JAVASCRIPT_FLAVOR }),
+    // Separator, index = 2
+    generateMockExtension({ category: ExtensionCategory.FRONTEND_FRAMEWORK }),
+    // Separator, index = 4
+    generateMockExtension({ category: ExtensionCategory.CSS_PREPROCESSOR }),
+    generateMockExtension({ category: ExtensionCategory.CSS_PREPROCESSOR }),
+  ]
+
+  prompts$.subscribe((question) => {
+    const choices = (question as CheckboxQuestion).choices as Array<
+      Choice | Separator
+    >
+    const separators = choices.filter(
+      (choice): choice is Separator => choice instanceof Separator,
+    )
+
+    expect(separators).toHaveLength(3)
+    expect(choices[0]).toHaveProperty('type', 'separator')
+    expect(choices[2]).toHaveProperty('type', 'separator')
+    expect(choices[4]).toHaveProperty('type', 'separator')
+
+    done()
+  })
+
+  selectExtensions(prompts$, answers$, extensions)
+})
--- src/core/userDialog/SelectExtensions.ts
+++ src/core/userDialog/SelectExtensions.ts
@@ -1,9 +1,17 @@
-import { Extension } from '../Extension'
+import { Extension, ExtensionCategory } from '../Extension'
 import { lastValueFrom, Observable, pluck, Subject, take } from 'rxjs'
 import { Answers, CheckboxQuestion, DistinctQuestion } from 'inquirer'
 import chalk from 'chalk'
 import { checkDependencies } from '../DependencyChecks'
 import { checkExclusivities } from '../ExclusivityChecks'
+import Separator from 'inquirer/lib/objects/separator'
+
+export const extensionCategoryTitles: Record<ExtensionCategory, string> = {
+  [ExtensionCategory.CSS_PREPROCESSOR]: 'CSS pre-processors',
+  [ExtensionCategory.FRONTEND_FRAMEWORK]: 'Frontend frameworks',
+  [ExtensionCategory.JAVASCRIPT_FLAVOR]: 'JavaScript Flavors',
+  [ExtensionCategory.ONLY_FOR_TESTING]: 'Only for testing, please ignore',
+}
 
 export const selectExtensions = (
   prompts$: Subject<DistinctQuestion>,
@@ -14,25 +22,65 @@ export const selectExtensions = (
     type: 'checkbox',
     name: 'chosenExtensions',
     message: 'What libraries / tools would you like to use in your project?',
-    choices: extensions.map((extension) => {
-      let name = `${chalk.bold(extension.name)} - ${
-        extension.description
-      } More info: ${chalk.underline(extension.linkToDocumentation.toString())}`
+    choices: extensions
+      // Ensure all extensions are grouped by category
+      .sort((a, b) => a.category - b.category)
+      // Add separators before each category
+      .reduce(
+        ({ prevCategory, extensionsWithSeparators }, extension) => {
+          const newExtensionsWithSeparators = [...extensionsWithSeparators]
 
-      if (extension.dependsOn) {
-        name += ` - ${chalk.italic.red(
-          `Depends on: ${extension.dependsOn
-            ?.map((dep) => dep.name)
-            .join(', ')}`,
-        )}`
-      }
+          // Also fires in the beginning, since prevCategory is initially undefined !== extension.category
+          if (prevCategory !== extension.category) {
+            newExtensionsWithSeparators.push(
+              new Separator(
+                `--- ${extensionCategoryTitles[extension.category]} ---`,
+              ),
+            )
+          }
 
-      return {
-        name,
-        value: extension,
-        short: extension.name,
-      }
-    }),
+          newExtensionsWithSeparators.push(extension)
+
+          return {
+            prevCategory: extension.category,
+            extensionsWithSeparators: newExtensionsWithSeparators,
+          }
+        },
+        {
+          prevCategory: undefined,
+          extensionsWithSeparators: [],
+        } as {
+          prevCategory: ExtensionCategory | undefined
+          extensionsWithSeparators: Array<Extension | Separator>
+        },
+      )
+      .extensionsWithSeparators.map((extensionOrSeparator) => {
+        // Keep separators, map extensions to questions
+        if (extensionOrSeparator instanceof Separator) {
+          return extensionOrSeparator
+        } else {
+          const extension = extensionOrSeparator
+          let name = `${chalk.bold(extension.name)} - ${
+            extension.description
+          } More info: ${chalk.underline(
+            extension.linkToDocumentation.toString(),
+          )}`
+
+          if (extension.dependsOn) {
+            name += ` - ${chalk.italic.red(
+              `Depends on: ${extension.dependsOn
+                ?.map((dep) => dep.name)
+                .join(', ')}`,
+            )}`
+          }
+
+          return {
+            name,
+            value: extension,
+            short: extension.name,
+          }
+        }
+      }),
     validate(
       chosenExtensions: Array<Extension>,
     ): boolean | string | Promise<boolean | string> {
