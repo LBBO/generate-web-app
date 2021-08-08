5b7795c8269fd0bb263547f0ec37cf901c0c4bba
Michael Kuckuk <mdk@kuckuk.com>
2021-08-08 15:08:00 +0200

    Create type for PackageManagerStrategy

---
+++ src/core/packageManagers/PackageManagerStrategy.ts
@@ -0,0 +1,13 @@
+export type PackageManagerStrategy = {
+  installDependencies: (
+    dependencies: Array<
+      string | { name: string; isDevDependency?: boolean; version?: string }
+    >,
+  ) => Promise<void>
+  checkInstalledStatus: (
+    packageName: string,
+  ) => Promise<
+    | { isInstalled: false }
+    | { isInstalled: true; isDevDependency: boolean; version: string }
+  >
+}
