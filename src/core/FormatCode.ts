d18b313bf72d51a2c166a89148880ccd270f0134
Michael Kuckuk <mdk@kuckuk.com>
2021-08-09 19:07:48 +0200

    Add helper method to format code with prettier

---
+++ src/core/FormatCode.ts
@@ -0,0 +1,37 @@
+import * as ts from 'typescript'
+import { format, Options as PrettierOptions } from 'prettier'
+
+export const defaultTypescriptCompilerOptions: ts.CompilerOptions = {
+  target: ts.ScriptTarget.ESNext,
+  jsx: ts.JsxEmit.Preserve,
+}
+export const defaultTypescriptTranspileOptions: ts.TranspileOptions = {
+  compilerOptions: defaultTypescriptCompilerOptions,
+}
+
+export const defaultPrettierOptions: PrettierOptions = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+}
+
+export const generatePrettierOptions = (
+  overrides: Partial<PrettierOptions> = {},
+): PrettierOptions => ({
+  ...defaultPrettierOptions,
+  ...overrides,
+})
+
+export const formatWithPrettier = (
+  fileContent: string,
+  filePath: string,
+  overrides: Partial<PrettierOptions> = {},
+): string => {
+  return format(
+    fileContent,
+    generatePrettierOptions({
+      ...overrides,
+      filepath: filePath,
+    }),
+  )
+}
