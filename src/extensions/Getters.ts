4f16c06647ccc0795bc027f1b8737c64f547d9a1
Michael Kuckuk <mdk@kuckuk.com>
2021-08-09 20:43:51 +0200

    Refactor extension getters

--- src/extensions/Getters.ts
+++ src/extensions/Getters.ts
@@ -7,7 +7,7 @@ import type { ESLintExtensionOptions } from './ESLintExtension'
 import type { ReactExtensionOptions } from './ReactExtension'
 import type { TypeScriptExtensionOptions } from './TypeScriptExtension'
 
-const getExtension =
+const createExtensionGetter =
   <ExtensionOptions extends Record<string, unknown>>(extensionName: string) =>
   (
     extensions: Array<Extension>,
@@ -17,54 +17,23 @@ const getExtension =
       | undefined
   }
 
-export const getAngularExtension = (
-  extensions: Array<Extension>,
-): ExtensionWithSpecificOptions<AngularExtensionOptions> | undefined =>
-  extensions.find((extension) => extension.name === 'Angular') as
-    | ExtensionWithSpecificOptions<AngularExtensionOptions>
-    | undefined
+export const getAngularExtension =
+  createExtensionGetter<AngularExtensionOptions>('Angular')
 
-export const getLessExtension = (
-  extensions: Array<Extension>,
-): ExtensionWithSpecificOptions<LessExtensionOptions> | undefined =>
-  extensions.find((extension) => extension.name === 'Less') as
-    | ExtensionWithSpecificOptions<LessExtensionOptions>
-    | undefined
+export const getLessExtension =
+  createExtensionGetter<LessExtensionOptions>('Less')
 
-export const getSassExtension = (
-  extensions: Array<Extension>,
-): ExtensionWithSpecificOptions<SassExtensionOptions> | undefined =>
-  extensions.find((extension) => extension.name === 'Sass') as
-    | ExtensionWithSpecificOptions<SassExtensionOptions>
-    | undefined
+export const getSassExtension =
+  createExtensionGetter<SassExtensionOptions>('Sass')
 
-export const getScssExtension = (
-  extensions: Array<Extension>,
-): ExtensionWithSpecificOptions<ScssExtensionOptions> | undefined =>
-  extensions.find((extension) => extension.name === 'SCSS') as
-    | ExtensionWithSpecificOptions<ScssExtensionOptions>
-    | undefined
+export const getScssExtension =
+  createExtensionGetter<ScssExtensionOptions>('SCSS')
 
-export const getESLintExtension = (
-  extensions: Array<Extension>,
-): ExtensionWithSpecificOptions<ESLintExtensionOptions> | undefined => {
-  return extensions.find((extension) => extension.name === 'ESLint') as
-    | ExtensionWithSpecificOptions<ESLintExtensionOptions>
-    | undefined
-}
+export const getESLintExtension =
+  createExtensionGetter<ESLintExtensionOptions>('ESLint')
 
-export const getReactExtension = (
-  extensions: Array<Extension>,
-): ExtensionWithSpecificOptions<ReactExtensionOptions> | undefined => {
-  return extensions.find((extension) => extension.name === 'React') as
-    | ExtensionWithSpecificOptions<ReactExtensionOptions>
-    | undefined
-}
+export const getReactExtension =
+  createExtensionGetter<ReactExtensionOptions>('React')
 
-export const getTypeScriptExtension = (
-  extensions: Array<Extension>,
-): ExtensionWithSpecificOptions<TypeScriptExtensionOptions> | undefined => {
-  return extensions.find((extension) => extension.name === 'TypeScript') as
-    | ExtensionWithSpecificOptions<TypeScriptExtensionOptions>
-    | undefined
-}
+export const getTypeScriptExtension =
+  createExtensionGetter<TypeScriptExtensionOptions>('TypeScript')
