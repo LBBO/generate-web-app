c5d9e951b2faecd0d9322de5911d69c1ad989fa5
Michael Kuckuk <mdk@kuckuk.com>
2021-08-18 10:22:26 +0200

    Install Angular using npx

--- src/extensions/AngularExtension/AngularSchematics.ts
+++ src/extensions/AngularExtension/AngularSchematics.ts
@@ -1,16 +1,17 @@
-import path from 'path'
 import { spawn } from 'child_process'
 
 export const addSchematic = (cwd: string, args: string[]): Promise<void> => {
   return new Promise((resolve, reject) => {
-    const nodeArgs = [
-      path.join(__dirname, '../../../node_modules/@angular/cli/bin/ng'),
+    const npxArgs = [
+      '-p=@angular/cli',
+      'ng',
+      'new',
       'add',
       '--skip-confirmation',
       ...args,
     ]
 
-    const childProcess = spawn('node', nodeArgs, { stdio: 'inherit', cwd })
+    const childProcess = spawn('npx', npxArgs, { stdio: 'inherit', cwd })
 
     childProcess.on('close', (statusCode) => {
       if (statusCode === 0) {
