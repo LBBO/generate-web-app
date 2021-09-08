293881826d22cff78a90082bc34fc83fc823f9c9
Michael Kuckuk <mdk@kuckuk.com>
2021-09-08 13:58:44 +0200

    Fix SelectExtensions tests

--- src/core/userDialog/SelectExtensions.test.ts
+++ src/core/userDialog/SelectExtensions.test.ts
@@ -1,13 +1,10 @@
-import { Subject } from 'rxjs'
-import type { Answers, CheckboxQuestion, DistinctQuestion } from 'inquirer'
-import { Separator } from 'inquirer'
+import type { CheckboxQuestion } from 'inquirer'
+import inquirer, { Separator } from 'inquirer'
 import { generateMockExtension } from '../../extensions/MockExtension'
 import { ExtensionCategory } from '../Extension'
 import { extensionCategoryTitles, selectExtensions } from './SelectExtensions'
 import Choice = require('inquirer/lib/objects/choice')
 
-let prompts$: Subject<DistinctQuestion>
-let answers$: Subject<Answers>
 const oneExtensionPerCategory = [
   generateMockExtension({
     category: ExtensionCategory.CSS_PREPROCESSOR,
@@ -30,67 +27,64 @@ const oneExtensionPerCategory = [
 ]
 type Separator = { line: string; type: 'separator' }
 
+let promptSpy: jest.SpyInstance
+
 beforeEach(() => {
-  prompts$ = new Subject()
-  answers$ = new Subject()
+  promptSpy = jest.spyOn(inquirer, 'prompt').mockResolvedValue({})
 })
 
 it(
   'should always ensure correct order of categories (Only for Testing, JS flavor, framework, JS' +
     ' lib, CSS pre-processor, linter / formatter)',
-  (done) => {
-    prompts$.subscribe((question) => {
-      const choices = (question as CheckboxQuestion).choices as Array<
-        Choice | Separator
-      >
-      const separators = choices.filter(
-        (choice): choice is Separator => choice instanceof Separator,
-      )
+  async () => {
+    await selectExtensions(oneExtensionPerCategory)
 
-      // Enums are transpiled to objects containing name: value, but also value: name
-      // Therefore, each actual enum value appears twice in the transpiled object
-      expect(separators).toHaveLength(Object.keys(ExtensionCategory).length / 2)
-      expect(separators[0].line).toMatch(
-        extensionCategoryTitles[ExtensionCategory.ONLY_FOR_TESTING],
-      )
-      expect(separators[1].line).toMatch(
-        extensionCategoryTitles[ExtensionCategory.JAVASCRIPT_FLAVOR],
-      )
-      expect(separators[2].line).toMatch(
-        extensionCategoryTitles[ExtensionCategory.FRONTEND_FRAMEWORK],
-      )
-      expect(separators[3].line).toMatch(
-        extensionCategoryTitles[ExtensionCategory.CSS_PREPROCESSOR],
-      )
-      expect(separators[4].line).toMatch(
-        extensionCategoryTitles[ExtensionCategory.JAVASCRIPT_LIBRARY],
-      )
-      expect(separators[5].line).toMatch(
-        extensionCategoryTitles[ExtensionCategory.LINTER_OR_FORMATTER],
-      )
+    expect(promptSpy).toHaveBeenCalledTimes(1)
 
-      done()
-    })
+    const questions = promptSpy.mock.calls[0][0]
+    expect(questions).toHaveLength(1)
 
-    selectExtensions(prompts$, answers$, oneExtensionPerCategory)
+    const question: CheckboxQuestion = questions[0]
+    const choices = question.choices as Array<Choice | Separator>
+    const separators = choices.filter(
+      (choice): choice is Separator => choice instanceof Separator,
+    )
+
+    // Enums are transpiled to objects containing name: value, but also value: name
+    // Therefore, each actual enum value appears twice in the transpiled object
+    expect(separators).toHaveLength(Object.keys(ExtensionCategory).length / 2)
+    expect(separators[0].line).toMatch(
+      extensionCategoryTitles[ExtensionCategory.ONLY_FOR_TESTING],
+    )
+    expect(separators[1].line).toMatch(
+      extensionCategoryTitles[ExtensionCategory.JAVASCRIPT_FLAVOR],
+    )
+    expect(separators[2].line).toMatch(
+      extensionCategoryTitles[ExtensionCategory.FRONTEND_FRAMEWORK],
+    )
+    expect(separators[3].line).toMatch(
+      extensionCategoryTitles[ExtensionCategory.CSS_PREPROCESSOR],
+    )
+    expect(separators[4].line).toMatch(
+      extensionCategoryTitles[ExtensionCategory.JAVASCRIPT_LIBRARY],
+    )
+    expect(separators[5].line).toMatch(
+      extensionCategoryTitles[ExtensionCategory.LINTER_OR_FORMATTER],
+    )
   },
 )
 
-it('should insert a separator before the first question', (done) => {
-  prompts$.subscribe((question) => {
-    const choices = (question as CheckboxQuestion).choices as Array<
-      Choice | Separator
-    >
-    expect(choices[0]).toHaveProperty('type', 'separator')
-    expect(choices[1]).not.toHaveProperty('type', 'separator')
+it('should insert a separator before the first question', async () => {
+  await selectExtensions(oneExtensionPerCategory)
 
-    done()
-  })
+  const question: CheckboxQuestion = promptSpy.mock.calls[0][0][0]
+  const choices = question.choices as Array<Choice | Separator>
 
-  selectExtensions(prompts$, answers$, oneExtensionPerCategory)
+  expect(choices[0]).toHaveProperty('type', 'separator')
+  expect(choices[1]).not.toHaveProperty('type', 'separator')
 })
 
-it('should insert a separator between two categories', (done) => {
+it('should insert a separator between two categories', async () => {
   const extensions = [
     // Separator, index = 0
     generateMockExtension({ category: ExtensionCategory.JAVASCRIPT_FLAVOR }),
@@ -101,21 +95,16 @@ it('should insert a separator between two categories', (done) => {
     generateMockExtension({ category: ExtensionCategory.CSS_PREPROCESSOR }),
   ]
 
-  prompts$.subscribe((question) => {
-    const choices = (question as CheckboxQuestion).choices as Array<
-      Choice | Separator
-    >
-    const separators = choices.filter(
-      (choice): choice is Separator => choice instanceof Separator,
-    )
+  await selectExtensions(extensions)
 
-    expect(separators).toHaveLength(3)
-    expect(choices[0]).toHaveProperty('type', 'separator')
-    expect(choices[2]).toHaveProperty('type', 'separator')
-    expect(choices[4]).toHaveProperty('type', 'separator')
+  const question: CheckboxQuestion = promptSpy.mock.calls[0][0][0]
+  const choices = question.choices as Array<Choice | Separator>
+  const separators = choices.filter(
+    (choice): choice is Separator => choice instanceof Separator,
+  )
 
-    done()
-  })
-
-  selectExtensions(prompts$, answers$, extensions)
+  expect(separators).toHaveLength(3)
+  expect(choices[0]).toHaveProperty('type', 'separator')
+  expect(choices[2]).toHaveProperty('type', 'separator')
+  expect(choices[4]).toHaveProperty('type', 'separator')
 })
