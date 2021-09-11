f31db9425fcb011e158587c2dc065fb7a2b2b7fc
Michael Kuckuk <mdk@kuckuk.com>
2021-09-11 18:50:27 +0200

    Test any array of configurations

--- e2e/FigureThingsOut.ts
+++ e2e/FigureThingsOut.ts
@@ -4,6 +4,7 @@
 
 import * as child_process from 'child_process'
 import chalk from 'chalk'
+import { PackageManagerNames } from '../src/core/packageManagers/PackageManagerStrategy'
 
 const asyncRunCommand = (entireCommand: string): Promise<void> => {
   return new Promise<void>((resolve, reject) => {
@@ -69,39 +70,113 @@ const stopContainer = async (containerID: string) => {
 
 const statusUpdate = chalk.white.inverse
 
-const testShit = async () => {
+type ConfigForInstallation = {
+  projectName: string
+  packageManager: PackageManagerNames
+  extensionOptions: string
+}
+
+/**
+ * Creates a new Docker container from the GWA image, attempts to install the given setup and tries to run `npm
+ * build` in the finished installation.
+ * @param config
+ * @returns installationWasSuccessful - True, iff the installation and build process succeeded
+ */
+const installAndBuildConfigInsideNewDockerContainer = async (
+  config: ConfigForInstallation,
+) => {
+  console.log(statusUpdate('Creating new container'))
+  const containerID = await startContainer('generate-web-app')
+  console.log(`Container ID is ${containerID}`)
+
+  let installationWasSuccessful = true
+
+  try {
+    console.log(statusUpdate(`Installing ${config.projectName}`))
+
+    const command = [
+      `gwa ${config.projectName}`,
+      `-p ${config.packageManager}`,
+      config.extensionOptions,
+    ].join(' ')
+    console.log(`Installation command: ${command}`)
+
+    await runInsideDockerContainer(containerID, command)
+
+    console.log(statusUpdate(`Attempting to build ${config.projectName}`))
+    await runInsideDockerContainer(
+      containerID,
+      'npm run build',
+      `/usr/src/generated/${config.projectName}`,
+    )
+    console.log(statusUpdate('Success'))
+  } catch (err) {
+    console.error(
+      chalk.inverse.red(`Installation of ${config.projectName} failed!`),
+    )
+    installationWasSuccessful = false
+  } finally {
+    console.log(statusUpdate('Stopping (which triggers deleting) container'))
+    await stopContainer(containerID)
+  }
+
+  return installationWasSuccessful
+}
+
+const runDockerTests = async () => {
   console.log(statusUpdate('Building image'))
   await buildDockerImage()
 
-  console.log(statusUpdate('Starting container'))
-  const containerID = await startContainer('generate-web-app')
-  try {
-    console.log(statusUpdate(`Generating app inside container ${containerID}`))
-    await runInsideDockerContainer(
-      containerID,
-      [
-        'gwa new-angular-scss-redux-eslint',
-        '-p yarn',
+  const configurations: Array<ConfigForInstallation> = [
+    {
+      projectName: 'bare-bone-react',
+      packageManager: PackageManagerNames.NPM,
+      extensionOptions: '--react --eslint',
+    },
+    {
+      projectName: 'angular-scss-redux-eslint',
+      packageManager: PackageManagerNames.YARN,
+      extensionOptions: [
         '--typescript --no-ts-strict-mode',
         '--angular --no-angular-routing',
         '--scss',
         '--redux',
         '--eslint',
       ].join(' '),
+    },
+  ]
+
+  const failedConfigurations: Array<ConfigForInstallation> = []
+
+  for (const configuration of configurations) {
+    const wasSuccessful = await installAndBuildConfigInsideNewDockerContainer(
+      configuration,
     )
-    console.log(statusUpdate('Running build'))
-    await runInsideDockerContainer(
-      containerID,
-      'npm run build',
-      '/usr/src/generated/new-angular-scss-redux-eslint',
+
+    if (!wasSuccessful) {
+      failedConfigurations.push(configuration)
+    }
+  }
+
+  console.log()
+  if (failedConfigurations.length === 0) {
+    console.log(
+      chalk.inverse.green('All configurations were installed successfully!'),
     )
-    console.log(statusUpdate('Success'))
-  } catch (err) {
-    console.error(chalk.inverse.red('Error: '), err)
-  } finally {
-    console.log(statusUpdate('Stopping (which triggers deleting) container'))
-    await stopContainer(containerID)
+  } else {
+    console.log(
+      chalk.inverse.red(
+        `${failedConfigurations.length} configurations failed:`,
+      ),
+    )
+
+    failedConfigurations.forEach((configuration, index) => {
+      console.log(
+        chalk.inverse(`${index + 1}.`),
+        JSON.stringify(configuration, null, 2),
+      )
+    })
   }
 }
 
-testShit()
+runDockerTests()
