9755964f6ed3af17773ecd24cd46425e83bc2815
Michael Kuckuk <mdk@kuckuk.com>
2021-09-12 18:19:29 +0200

    Remove unused function

--- e2e/DockerHelper.ts
+++ e2e/DockerHelper.ts
@@ -3,18 +3,6 @@ import { asyncRunCommand } from '../src/core/Utils'
 export const buildDockerImage = (): Promise<void> =>
   asyncRunCommand('docker build . -t generate-web-app')
 
-export const createDockerContainerAndRun = (command: string): Promise<void> =>
-  asyncRunCommand(
-    'docker run ' +
-      // Give the resulting container the name 'test-gwa'
-      '--name test-gwa ' +
-      '-w /usr/src/generated ' +
-      // Create the container from the 'generate-web-app' image
-      'generate-web-app ' +
-      // Run this command
-      command,
-  )
-
 export const runInsideDockerContainer = (
   containerName: string,
   command: string,
